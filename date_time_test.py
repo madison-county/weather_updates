5b14d337b0f0b245f2c6178fed0f1bcb520fd956
boykjgis <boykjgis@gmail.com>
2022-12-02 08:47:00 -0700

    date_time_test.py: Add dictionary and user input for searching locations

--- date_time_test.py
+++ date_time_test.py
@@ -13,3 +13,23 @@ print(current_day.strftime('%Y/%d/%m-%H:%M'))
 date_str = current_day.strftime('%Y/%d/%m-%H:%M')
 print(type(date_str))
 print(date_str)
+
+locations = {
+    "alder" : "45.3238072222328, -112.10734772283459",
+    "laurin" : "45.35271239238317, -112.1177774887102",
+    "sheridan" : "45.45534588182824, -112.19707886464111",
+    "twin bridges" : "45.54419121141109, -112.33107814032275"
+}
+
+for val in locations.items():
+   print(val)
+
+loc_entry = input("Enter a location: \n")
+#print("You entered %s" % loc_entry)
+
+if loc_entry.lower() in locations:
+    print("{0} found in the dictionary".format(loc_entry.upper()))
+
+#for i in range(len(locations)):
+#    for key, val in locations[i].items():
+#        print("{0} : {1}".format(key, str(val)))
